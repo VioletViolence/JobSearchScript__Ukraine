95697118eb03b7029b8e7cc05b2bda915215f79b
Violet Violence <brosinskiy@gmail.com>
2020-01-06 02:10:15 +0200

    First step(Rabota.ua opening and entering a value)

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -1,4 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class MainController {
     public static void main(String[] args) {
@@ -7,6 +12,18 @@ public class MainController {
 
         driver.get("http://rabota.ua/jobsearch");
 
+        WebElement input = driver.findElement(By.xpath("//input[@placeholder='Введите ключевые слова']"));
+        input.sendKeys("Junior Java Developer");
+
+        Actions builder = new Actions(driver);
+
+        builder.sendKeys(Keys.ENTER).build().perform();
+
+        WebElement foundDriver = driver.findElement(By.className("fd-fat-merchant"));
+
+        if(Integer.parseInt(foundDriver.getText()) >= 1){
+            System.out.println("Ama big boi and I do automatization");
+        }
 
     }
 }
